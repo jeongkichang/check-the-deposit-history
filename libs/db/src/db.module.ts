52f905670c2bc430ea00a50d1ac359969bacaca3
niche <light-hearted@naver.com>
2025-02-05 19:17:10 +0900

    feat: init mongo db

---
+++ libs/db/src/db.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGO_URI') || 'mongodb://localhost:27017/dbname',
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    exports: [
+        MongooseModule,
+    ],
+})
+export class DbModule {}
