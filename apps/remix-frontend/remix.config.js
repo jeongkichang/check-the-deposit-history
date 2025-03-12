037eeccadac0f40f8e5064e4009e772be07ceb42
jeongkichang <light-hearted@naver.com>
2025-03-12 21:44:00 +0900

    안 쓰는 파일 삭제

--- apps/remix-frontend/remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    appDirectory: "app",
-    serverBuildPath: "build/index.js",
-    serverModuleFormat: "cjs",
-    devServerPort: 8002,
-    ignoredRouteFiles: ["**/.*"],
-};
