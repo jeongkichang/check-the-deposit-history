335954bc7b4530bc49326c9551f6542ac4df4bc2
niche <light-hearted@naver.com>
2025-04-17 19:14:28 +0900

    농협 html의 비밀번호는 생년월일도 있지만, 사업자등록번호일 수도 있기 때문에, OAuth 2.0에 생년월일 조회 권한을 별도로 요청하지 않는다.

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -322,7 +322,6 @@ export class ApiController {
         const scopes = [
             'https://www.googleapis.com/auth/gmail.readonly',
             'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/user.birthday.read',
         ];
 
         const url = this.oauth2Client.generateAuthUrl({
